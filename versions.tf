a9b585e31ba698e245d078ace18680860a8ba53a
Salim Afiune Maya <afiune@lacework.net>
2023-11-30 15:34:52 -0800

    chore: allow AWS provider >= v5
    We are not aware of any limitation.
    
    https://lacework.atlassian.net/browse/LINK-2317
    
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.15.1"
 
   required_providers {
-    aws    = {
-      source = "hashicorp/aws"
-      version = ">= 3.0, < 5.0.0"
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 3.0"
       configuration_aliases = [aws.audit, aws.log_archive]
     }
     random = ">= 2.1"
